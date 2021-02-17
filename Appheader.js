e17ccf2d52876288c1f846eab3db8bf91a56788c
mopot789 <76504676+mopot789@users.noreply.github.com>
2021-02-17 23:44:47 +0700

    Add files via upload

---
+++ Appheader.js
@@ -0,0 +1,13 @@
+function Appheader(){
+
+
+return (
+   
+      <header className="App-header" src ="/pic/header-logo.jpg">
+          <img className = "header-logo" src ="/pic/header-logo.jpg"></img>
+
+       
+      </header>
+      );
+    }
+export default Appheader;
---
+++ items.js
@@ -0,0 +1,28 @@
+const items =[
+    {
+      title :"man",
+      thumbnaiUrl :"/pic/man.jpg"
+      
+    
+    },
+    {
+      title :"girl",
+      thumbnaiUrl :"/pic/girl.jpg"
+      
+    
+    },
+    {
+      title :"boy",
+      thumbnaiUrl :"/pic/boy.jpg"
+      
+    
+    },
+    {
+      title :"view",
+      thumbnaiUrl :"/pic/aurora.jpg"
+      
+    
+    }
+  ];
+  export default items;
+  
